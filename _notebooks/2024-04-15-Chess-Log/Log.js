78058cec03dbf116689d177cd3b09fa5afbd16e8
jkaeshim <Jak3shim@gmail.com>
2024-04-17 15:20:16 -0700

    Chess2

--- _notebooks/2024-04-15-Chess-Log/Log.js
+++ _notebooks/2024-04-15-Chess-Log/Log.js
@@ -2,10 +2,10 @@ function logLastMove(attackList) {
     if (attackList.length > 0) {
         var lastMove = attackList[attackList.length - 1];
         var moveLogDiv = document.getElementById("moveLog");
-        moveLogDiv.innerHTML += `<br>Last move: ${lastMove}`;  // Appends the last move to the log
+        moveLogDiv.innerHTML += `<br>Last move: ${lastMove}`;  
     }
 }
 
 // Example move logging
-var attackList = [[0, 1], [1, 2], [2, 3]];  // Example coordinates of moves
-logLastMove(attackList);  // Call this function whenever a move is made
+var attackList = [[0, 1], [1, 2], [2, 3]];  
+logLastMove(attackList);  
